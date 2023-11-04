9786bd2daf34ea1fc0e02e7e71343debab18fdd7
Ishi Rastogi <ishi.rastogi@icloud.com>
2023-11-04 16:46:14 +0530

    Added Text Transform

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Textform from "./components/TextForm";
+/*import about from './components/navbar';*/
+import react, { useState } from "react";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState({msg:"",type:""});
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Textform
+          showAlert={showAlert}
+          heading="Enter the text to analyze"
+          mode={mode}
+        />
+      </div>
+      {/*<about mode={mode}/>*/}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,60 @@
+import React,{useState} from 'react'
+
+export default function Textform(props) {
+  const handleUpClick=()=>{
+    /* console.log("Uppercase was clicked" + text);*/
+     let newText= text.toUpperCase();
+     setText(newText)
+     props.showAlert("convertd to uppercase","success");
+   
+ 
+    
+   
+   }
+   const handleLowClick=()=>{
+    let newText= text.toLowerCase();
+    setText(newText)
+    props.showAlert("convertd to lowercase","success");
+
+}
+const handleClearClick=()=>{
+ let newText= '';
+ setText(newText)
+ props.showAlert("text cleard", "success")
+}
+ const handleExtraSpace=()=>{
+   let newText= text.split(/[]+/);
+   setText(newText.join(""))
+   props.showAlert("Handle extra spaces", "success")
+ }
+
+
+ const handleOnChange=(event)=>{
+  /* console.log("on change");*/
+   setText(event.target.value)
+}
+const[text, setText]= useState('');
+   return (
+    <>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1>{props.heading}</h1>
+<div className="mb-3">
+      <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'black'}} id="mybox" rows="6"></textarea>
+</div>
+<button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to Lowercase</button>
+<button className='btn btn-primary mx-1 my-1' onClick={handleClearClick}>Convert to ClearCase</button>
+<button className="btn btn-primary mx-1 my-1"> onClick={handleExtraSpace}Remove ExtraSpace</button>
+
+    </div>
+    <div className="container" my-3  style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").filter(w=>w !== "").length} words and {text.length} characters </p>
+      <p>{0.008*text.split("").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Nothing to preview"}</p>
+    </div>
+    </>
+  )
+}
+  
